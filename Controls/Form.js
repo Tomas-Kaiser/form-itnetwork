be102a8a088ca37d21214d21bc0ad8ef70d48988
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-02-03 23:00:35 +0000

    Added design

--- Controls/Form.js
+++ Controls/Form.js
@@ -25,33 +25,50 @@ class Form extends SimplexControl {
       ];
 
       this._addUnorderList();
-
       this._addListItems();
    }
 
    _addUnorderList() {
       // Create unorder list for main menu
-      let menuItems = this.element.append('<ul></ul>').getNewElements();
+      let menuItems = this.element.append('<ul class="main-menu"></ul>').getNewElements();
 
       // Iterate through array this.mainMenu and create list itmes in it
       for (const menu of this.form) {
-         menuItems.append(`<li id='${menu.mainMenu.toLowerCase()}'>${menu.mainMenu}</li>`).getNewElements();
+         menuItems.append(`<li id='${menu.mainMenu.toLowerCase()}' class="main-li">${menu.mainMenu}<p>Paragraph</p></li>`).getNewElements();
       }
+
+
    }
 
    _addListItems() {
       for (const item of this.form) {
          // Select list item from mainMenu
-         let selectLi = this.element.select(`#${item.mainMenu.toLocaleLowerCase()}`);
-         selectLi.append('<ul class="hidden"></ul>').getNewElements();
-         
+         let selectMainMenuLi = this.element.select(`#${item.mainMenu.toLocaleLowerCase()}`);
+         selectMainMenuLi.append('<ul class="hidden sub-menu-ul"></ul>').getNewElements();
+
          // Select li of parent ul
-         let selectSubMenuLi = selectLi.children;
+         let selectSubMenuUl = selectMainMenuLi.children.last;
 
          // iterate array subMenu
          for (let subItem of item.subMenu) {
-            selectSubMenuLi.append(`<li>${subItem}</li>`)
+            selectSubMenuUl.append(`<li>${subItem}<p>Paragraph</p></li>`);
          }
+
+         // Add even listener for mainMenu
+         selectMainMenuLi.onClick(() => {
+            if (selectSubMenuUl.hasClass('hidden')) {
+               selectSubMenuUl.removeClass('hidden');
+            } else {
+               selectSubMenuUl.addClass('hidden');
+            }
+            console.log("li clicked")
+         });
+
+         // Stop proapgation for nested li
+         selectSubMenuUl.onClick((sender, e) => {
+            console.log("Sub Li was clicked ")
+            e.stopPropagation();
+         });
       }
    }
 }
